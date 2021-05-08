0b2e39429701490c4ece8e9c3dda3488f23841da
accidentlywoo <>
2021-05-09 00:50:27 +0900

    ? : .git 중첩 날림

---
+++ practice/src/test/kotlin/chapter2/IterationTest.kt
@@ -0,0 +1,29 @@
+package chapter2
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class IterationTest{
+	@Test
+	fun `map` () {
+		val binaryReps = TreeMap<Char, String>()
+
+		for (c in 'A'..'F') {
+			val binary = Integer.toBinaryString(c.toInt())
+			binaryReps[c] = binary
+		}
+
+		for ((letter, binary) in binaryReps) {
+			println("$letter = $binary")
+		}
+	}
+
+	@Test
+	fun `collection` () {
+		val list = arrayListOf("10", "11", "1001")
+		for ((index, element) in list.withIndex()) {
+			println("$index: $element")
+		}
+	}
+}
