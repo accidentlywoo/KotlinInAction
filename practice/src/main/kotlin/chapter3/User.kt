c4986e637512fe03662d116f539dff8ba1eb09a2
accidentlywoo <accidentlywoo@gmail.com>
2021-05-11 14:30:54 +0900

    refactor : 필요없는 파라미터 제거

--- practice/src/main/kotlin/chapter3/User.kt
+++ practice/src/main/kotlin/chapter3/User.kt
@@ -5,13 +5,12 @@ import java.lang.IllegalArgumentException
 class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
-
-	fun validate(user: User, value: String, fieldName: String) {
+	fun validate(value: String, fieldName: String) {
 		if (value.isEmpty())
 			throw IllegalArgumentException("Can't save user ${user.id}: empty Name")
 	}
 
-	validate(user, user.name, "Name")
-	validate(user, user.address, "Address")
+	validate(user.name, "Name")
+	validate(user.address, "Address")
 
 }
